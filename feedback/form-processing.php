b4f7deefff38ca9cfeeda374c055c8e39298f309
Aleksandersie <97347897+Aleksandersie@users.noreply.github.com>
2022-06-27 23:35:51 +0300

    Update form-processing.php

--- feedback/form-processing.php
+++ feedback/form-processing.php
@@ -38,7 +38,7 @@ define('HAS_SEND_EMAIL', true);
 // добавить ли прикреплённые файлы в тело письма в виде ссылок
 define('HAS_ATTACH_IN_BODY', true);
 define('EMAIL_SETTINGS', [
-  'addresses' => ['manager@domain.com'], // кому необходимо отправить письмо
+  'addresses' => ['aleksander.sie@gmail.com'], // кому необходимо отправить письмо
   'from' => ['no-reply@domain.com', 'Имя сайта'], // от какого email и имени необходимо отправить письмо
   'subject' => 'Сообщение с формы обратной связи', // тема письма
   'host' => 'ssl://smtp.yandex.ru', // SMTP-хост
